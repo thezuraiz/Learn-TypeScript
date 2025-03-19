b1a76a00317bfbb374fa6d25530f1389019e5b47
The Zuraiz <thezuraiz@gmail.com>
2025-03-19 15:40:57 +0500

    Learn Types

---
+++ intro.js
@@ -0,0 +1,14 @@
+var user = { name: "Zuraiz Khan", email: "thezuraiz@gmail.com" };
+console.log("username", user.name);
+var transaction = {
+    transactionId: 1 - 23123,
+    senderId: 121 - 2312,
+    receiverId: 123 - 23121,
+};
+var bankAccount = {
+    accountId: 123213,
+    accoundHolder: "Zuraiz Khan",
+    balance: 2312,
+    isActive: true,
+    transactions: [transaction, transaction],
+};
---
+++ intro.ts
@@ -0,0 +1,74 @@
+/// TYPE
+// ========================================================================
+
+let user = { name: "Zuraiz Khan", email: "thezuraiz@gmail.com" };
+console.log("username", user.name);
+// console.log('username',user.age);
+
+// type User = {
+//     name: string;
+//     age: number;
+//     phoneNumber: string;
+//   };
+
+//   let user1: User = {
+//     name: "Zuraiz Khan",
+//     age: 20,
+//     phoneNumber: "03001234029",
+//   };
+//   let user2: User = {
+//     name: "Abdullah",
+//     age: 15,
+//     phoneNumber: "03001234567",
+//   };
+
+//   let printUser = (users: User[]): void => {
+//     users.map((e) => {
+//       console.log(e.age);
+//     });
+//   };
+
+//   printUser([user1, user2]);
+
+// ========================================================================
+/// Interface
+
+// interface Transactions {
+//   transactionId: number;
+//   senderId: number;
+//   receiverId: number;
+// }
+// interface BankAccount {
+//   accountId: number;
+//   accoundHolder: string;
+//   balance: number;
+//   isActive: boolean;
+//   transactions: Transactions[];
+// }
+
+// let transaction: Transactions = {
+//   transactionId: 1 - 23123,
+//   senderId: 121 - 2312,
+//   receiverId: 123 - 23121,
+// };
+// const bankAccount: BankAccount = {
+//   accountId: 123213,
+//   accoundHolder: "Zuraiz Khan",
+//   balance: 2312,
+//   isActive: true,
+//   transactions: [transaction, transaction],
+// };
+
+interface Book {
+  name: string;
+  publishDate: string;
+  price: number;
+}
+let book: Book = {
+  name: "Atomic Habbit",
+  publishDate: Date.now.toString(),
+  price: 3000,
+};
+// console.log(`Book: ${book.name}`);
+
+interface Ebook {}
