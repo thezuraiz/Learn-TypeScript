bb40bd868a3d3116ef90422808b2562cad07b3e4
The Zuraiz <thezuraiz@gmail.com>
2025-03-20 12:55:50 +0500

    Do not use Any

---
+++ dontUseAny.ts
@@ -0,0 +1,13 @@
+// let anything; // by default it uses type ANY: Hover on it to see warning
+// function greetings() {
+//   return "true";
+// }
+// anything = true;
+
+let anything: bool; // Now It shows error That is recommended
+function greetings() {
+  return "true";
+}
+anything = greetings();
+
+// Dont Use Any
