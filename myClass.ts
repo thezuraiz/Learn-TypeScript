3de527db66904eeea0262e3d5609a27a81ce9d12
The Zuraiz <thezuraiz@gmail.com>
2025-03-21 16:26:41 +0500

    Class in TypesScript

--- myClass.ts
+++ myClass.ts
@@ -1,9 +1,10 @@
 class UserClass {
-  email: string;
+  email: string; // Always mention attributes in TS
   username: string;
   readonly password: string;
 
   constructor(email: string, passwords: string) {
+    // Always mention parameter types
     this.email = email;
     this.username = email.split("@").toString();
     this.password = `changeMyPassword${this.email}`;
