774fcbf49d3257cbe3e7613aa9292f36d36196d9
The Zuraiz <thezuraiz@gmail.com>
2025-03-21 15:45:34 +0500

    Interface vs Type Allias

--- myInterface.ts
+++ myInterface.ts
@@ -6,7 +6,15 @@ interface User {
   getCoupons(couponName: string, value: number): string;
 }
 
-let mostNewUser: User = {
+// In Interface You can inherit classes using extend Keyword, You can change its type
+// In Type Aliase, you use & sign, You can't change its type
+
+interface Admin extends User {
+  role: "Manager" | "Team Lead" | "Senior Dev";
+}
+let mostNewUser: Admin = {
+  // Now you need to pass new + old variable
+  role: "Manager",
   userId: 23230,
   email: "thezuraiz@gmail.com",
   startTrial() {
